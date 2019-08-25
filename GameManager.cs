6b039d3f55dd553fb9b29931bad3d26de289c1b1
Anshu Nair <anair14@myseneca.ca>
2019-08-24 23:27:00 -0400

    PlacePlayerComplete

--- GameManager.cs
+++ GameManager.cs
@@ -7,17 +7,22 @@ namespace SA
     public class GameManager : MonoBehaviour
     {
         public int maxHeight = 15;
-        public int maxWidth = 15;
+        public int maxWidth = 17;
 
         public Color color1;
         public Color color2;
+        public Color playerColor = Color.black;
 
+        GameObject playerObj;
         GameObject mapObject;
         SpriteRenderer mapRenderer;
+
+        Node[,] grid;
 
         private void Start()
         {
             CreateMap();
+            PlacePlayer();
         }
 
         void CreateMap()
@@ -25,12 +30,27 @@ namespace SA
             mapObject = new GameObject("map");
             mapRenderer = mapObject.AddComponent<SpriteRenderer>();
 
+            grid = new Node[maxWidth, maxHeight];
+
             Texture2D txt = new Texture2D(maxWidth, maxHeight);
 
             for (int i = 0; i < maxWidth; i++)
             {
                 for (int j = 0; j < maxHeight; j++)
                 {
+                    Vector3 tp = Vector3.zero;
+                    tp.x = i;
+                    tp.y = j;
+
+                    Node n = new Node()
+                    {
+                        x = i,
+                        y = j,
+                        worldPosition = tp
+                    };
+
+                    grid[i, j] = n;
+
                     #region Visual
                     if (i % 2 != 0)
                     {
@@ -63,6 +83,34 @@ namespace SA
             Rect rect = new Rect(0, 0, maxWidth, maxHeight);
             Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
             mapRenderer.sprite = sprite;
+        }
+
+        Node GetNode(int x, int y)
+        {
+            if (x < 0 || x > maxWidth - 1 || y < 0 || y > maxHeight - 1)
+                return null;
+
+            return grid[x, y];
+        }
+
+        void PlacePlayer()
+        {
+            playerObj = new GameObject("Player");
+            SpriteRenderer playerRender = playerObj.AddComponent<SpriteRenderer>();
+            playerRender.sprite = CreateSprite(playerColor);
+            playerRender.sortingOrder = 1;
+
+            playerObj.transform.position = GetNode(3, 3).worldPosition;
+        }
+
+        Sprite CreateSprite(Color targetColor)
+        {
+            Texture2D txt = new Texture2D(1, 1);
+            txt.SetPixel(0, 0, targetColor);
+            txt.filterMode = FilterMode.Point;
+            txt.Apply();
+            Rect rect = new Rect(0, 0, 1, 1);
+            return Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
         }
     }
 }
