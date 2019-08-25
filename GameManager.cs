90598d6104abb1836faaf1cedd01024e8bb00c8b
Anshu Nair <anair14@myseneca.ca>
2019-08-25 18:09:35 -0400

    movePlayerComplete

--- GameManager.cs
+++ GameManager.cs
@@ -13,18 +13,31 @@ namespace SA
         public Color color2;
         public Color playerColor = Color.black;
 
+        public Transform cameraHolder;
+
         GameObject playerObj;
         GameObject mapObject;
         SpriteRenderer mapRenderer;
 
+        Node playerNode;
         Node[,] grid;
 
+        bool up, left, right, down;
+        bool movePlayer;
+
+        Direction curDirection;
+        public enum Direction
+        {
+            up,down,left,right
+        }
+
+        #region Init
         private void Start()
         {
             CreateMap();
             PlacePlayer();
+            PlaceCamera();
         }
-
         void CreateMap()
         {
             mapObject = new GameObject("map");
@@ -81,28 +94,112 @@ namespace SA
             txt.filterMode = FilterMode.Point;
             txt.Apply();
             Rect rect = new Rect(0, 0, maxWidth, maxHeight);
-            Sprite sprite = Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+            Sprite sprite = Sprite.Create(txt, rect, Vector2.zero, 1, 0, SpriteMeshType.FullRect);
             mapRenderer.sprite = sprite;
         }
-
-        Node GetNode(int x, int y)
-        {
-            if (x < 0 || x > maxWidth - 1 || y < 0 || y > maxHeight - 1)
-                return null;
-
-            return grid[x, y];
-        }
-
         void PlacePlayer()
         {
             playerObj = new GameObject("Player");
             SpriteRenderer playerRender = playerObj.AddComponent<SpriteRenderer>();
             playerRender.sprite = CreateSprite(playerColor);
             playerRender.sortingOrder = 1;
+            playerNode = GetNode(3, 3);
+            playerObj.transform.position = playerNode.worldPosition;
+        }
+        void PlaceCamera()
+        {
+            Node n = GetNode(maxWidth / 2, maxHeight / 2);
+            Vector3 p = n.worldPosition;
+            p += Vector3.one * .5f;
+            cameraHolder.position = p;
+        }
+        #endregion
 
-            playerObj.transform.position = GetNode(3, 3).worldPosition;
+        #region Update        
+        private void Update()
+        {
+            GetInput();
+            SetPlayerDirection();
+            MovePlayer();
         }
 
+        void GetInput()
+        {
+            up = Input.GetButtonDown("Up");
+            left = Input.GetButtonDown("Left");
+            right = Input.GetButtonDown("Right");
+            down = Input.GetButtonDown("Down");
+        }
+        void SetPlayerDirection()
+        {
+            if (up)
+            {
+                curDirection = Direction.up;
+                movePlayer = true;
+            }
+            else if (down)
+            {
+                curDirection = Direction.down;
+                movePlayer = true;
+            }
+            else if (left)
+            {
+                curDirection = Direction.left;
+                movePlayer = true;
+            }
+            else if (right)
+            {
+                curDirection = Direction.right;
+                movePlayer = true;
+            }
+        }
+        void MovePlayer()
+        {
+            if (!movePlayer)
+                return;
+
+            movePlayer = false;
+
+            int x = 0;
+            int y = 0;
+
+            switch (curDirection)
+            {
+                case Direction.up:
+                    y = 1;
+                    break;
+                case Direction.down:
+                    y = -1;
+                    break;
+                case Direction.left:
+                    x = -1;
+                    break;
+                case Direction.right:
+                    x = 1;
+                    break;
+            }
+
+            Node targetNode = GetNode(playerNode.x + x, playerNode.y + y);
+            if (targetNode == null)
+            {
+                //Game Over
+            }
+            else
+            {
+                playerObj.transform.position = targetNode.worldPosition;
+                playerNode = targetNode;
+            }
+        }
+        #endregion
+
+        #region Utilities
+        Node GetNode(int x, int y)
+        {
+            if (x < 0 || x > maxWidth - 1 || y < 0 || y > maxHeight - 1)
+                return null;
+
+            return grid[x, y];
+        }       
         Sprite CreateSprite(Color targetColor)
         {
             Texture2D txt = new Texture2D(1, 1);
@@ -110,7 +207,8 @@ namespace SA
             txt.filterMode = FilterMode.Point;
             txt.Apply();
             Rect rect = new Rect(0, 0, 1, 1);
-            return Sprite.Create(txt, rect, Vector2.one * .5f, 1, 0, SpriteMeshType.FullRect);
+            return Sprite.Create(txt, rect, Vector2.zero, 1, 0, SpriteMeshType.FullRect);
         }
+        #endregion
     }
 }
